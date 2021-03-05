7ab17ba01c7b7c45c2540133655d5b12ae3a51c6
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-03-05 13:03:31 +0100

    Made endpoints and updated models

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,14 +1,118 @@
-const user = require("./userModel.js");
-const booking = require("./bookingModel.js");
-const apartment = require("./apartmentModel.js");
-const amenities = require("./amenitiesModel.js");
-const availableDates = require("./availableDatesModel.js");
+const mongoose = global.mongoose;
 
+const User = mongoose.model("User", {
+  /*id: {
+    type: String,
+    required: true,
+    unique: true,
+  },*/
+  fullName: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Apartment = mongoose.model("Apartment", {
+  /*id: {
+    type: String,
+    required: true,
+    unique: true,
+  },*/
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  pricePerDay: {
+    type: Number,
+    required: true,
+  },
+
+  city: {
+    type: String,
+    required: true,
+  },
+  region: String,
+});
+
+const Booking = mongoose.model("Booking", {
+  /*id: {
+    type: String,
+    required: true,
+    unique: true,
+  },*/
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  apartmentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Apartment",
+    required: true,
+  },
+
+  startDate: {
+    type: Number,
+    required: true,
+  },
+  endDate: {
+    type: Number,
+    required: true,
+  },
+});
+
+const AvailableDates = mongoose.model("AvailableDates", {
+  /*id: {
+    type: String,
+    required: true,
+    unique: true,
+  },*/
+  apartmentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Apartment",
+    required: true,
+  },
+  availableStartDate: {
+    type: Number,
+    required: true,
+  },
+  availableEndDate: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Amenities = mongoose.model("Amenities", {
+ /* id: {
+    type: String,
+    required: true,
+    unique: true,
+  },*/
+
+  apartmentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Apartment",
+    required: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+  description: String,
+});
 
 module.exports = {
-  users: user,
-  bookings: booking,
-  apartments: apartment,
-  amenities: amenities,
-  availableDates: availableDates,
+  users: User,
+  apartments: Apartment,
+  amenities: Amenities,
+  availableDates: AvailableDates,
+  bookings: Booking,
 };
--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,49 @@ const express = require("express");
 const app = express();
 const models = require("./models/models.js");
 
-
 app.use(express.json());
 
 const atlasUrl =
   "mongodb+srv://admin:pass123!@cluster0.qob2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-  
 global.mongoose.connect(atlasUrl, {
-  // let me speak the same dialect
-  // as a modern MongoDB server:
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+//Getter for all models except for users
+app.get("/rest/:model", async (req, res) => {
+  let model = models[req.params.model];
+  if (req.params.model === "users") {
+    res.json("No such request is found");
+    return;
+  }
 
+  let docs = await model.find();
+  res.json(docs);
+});
 
+//Post for all models
+app.post("/rest/:model", async (req, res) => {
+  let model = models[req.params.model];
 
+  let doc = new model(req.body);
+  await doc.save();
 
+  res.json(doc);
+});
 
-app.listen(3001, () => console.log('Server stated on port 3001'))
+//Put for all models
+app.put("/rest/:model/:id", async (req, res) => {
+  let model = models[req.params.model];
+
+  let doc = await model.findById(req.params.id);
+
+  // update object with new values
+  Object.assign(doc, req.body);
+  await doc.save();
+
+  res.json(doc);
+});
+
+app.listen(3001, () => console.log("Server stated on port 3001"));
