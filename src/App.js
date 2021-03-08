c99d03fbdf874fb1b9a2b70eca38aadc280607f9
Alexander S <70532114+autumnjava@users.noreply.github.com>
2021-03-08 15:22:26 +0100

    router-switch links

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import Nav from './components/Nav'
 import Home from './pages/Home.js'
 import Footer from './components/Footer'
 
+// Libs
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const page404 = () => (
+  <h1>Page not found: {window.location.pathname}</h1>
+)
+
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Home />
-      <Footer />
+      <Router>
+        <Nav />
+
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="*" component={page404} />
+          </Switch>
+        </main>
+
+        <footer>
+          <Footer />
+        </footer>
+      </Router>
     </div>
   );
 }
