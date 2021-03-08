e05cdcb478c9e3eff9de1e422d4d163f71873669
Alexander S <70532114+autumnjava@users.noreply.github.com>
2021-03-08 15:32:58 +0100

    important fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import '../css/Footer.css'
 export default function Footer() {
   return (
     <div className="footer">
-      <p>© 2020 clearbnb</p>
+      <p>© 2021 clearbnb</p>
       <p>Privacy · Terms · Sitemap · Company Details · About us</p>
     </div>
   )
