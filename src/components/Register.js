b27b603c27d43f206883c1563bd53c88923c1a8c
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-03-09 14:19:14 +0100

    Sending new user to collection

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,23 +1,94 @@
-import '../css/Register.css'
+import "../css/Register.css";
+import React, { useState } from "react";
 
 export default function Register() {
+ const addUser = async (user) => {
+   let res = await fetch("/rest/users", {
+     method: "POST",
+     headers: { "content-type": "application/json" },
+     body: JSON.stringify(user),
+   });
+
+   res = await res.json();
+   user.id = res.id;
+
+   console.log(res);
+ };
+
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dataIsMissing, setDataIsMissing] = useState(false);
+  const toggle = React.useCallback(() => setDataIsMissing(true));
+
+  function registerNewUser(e) {
+    e.preventDefault();
+    if (fullName === "" || email === "" || password === "") {
+      toggle();
+      console.log(dataIsMissing);
+      console.log("no right data");
+
+      setFullName("");
+      setEmail("");
+      setPassword("");
+
+      return;
+    }
+
+    setFullName("");
+    setEmail("");
+    setPassword("");
+
+    const newUser = {
+      fullName: fullName,
+      email: email,
+      password: password,
+    };
+
+    console.log(newUser);
+
+    addUser(newUser)
+  }
+
   return (
     <div className="register">
       <p className="registerText">Register new user</p>
       <div>
         <label>Full name</label>
-        <input placeholder="  Type your full name..."></input>
+        <input
+          key="1"
+          type="text"
+          required
+          placeholder="  Type your full name..."
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+        ></input>
       </div>
       <div>
         <label>Email</label>
-        <input className="emailInput" placeholder="  Type your email..."></input>
+        <input
+          key="2"
+          type="text"
+          required
+          className="emailInput"
+          placeholder="  Type your email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
       </div>
       <div>
         <label>Password</label>
-        <input placeholder="  Type your password..."></input>
+        <input
+          key="3"
+          type="text"
+          required
+          placeholder="  Type your password..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
       </div>
       <div className="register_button">
-        <button>Register</button>
+        <button onClick={registerNewUser}>Register</button>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import CategoryCards from '../components/homecomps/CategoryCards'
 import HomesHomepageDisplay from '../components/homecomps/HomesHomepageDisplay'
 import LocationCards from '../components/homecomps/LocationsCards'
 import '../css/Home.css'
+import Register from '../components/Register'
 
 export default function Home() {
   return (
@@ -14,6 +15,10 @@ export default function Home() {
       <div className="break"></div>
       <HomesHomepageDisplay />
       <div className="break lastbreak"></div>
+
+      <div>
+        <Register/>
+      </div>
     </div>
   )
 }
