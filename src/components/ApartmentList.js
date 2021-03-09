245c01232718a2d53e11046b7009cafedcd17bc7
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-03-09 16:25:50 +0100

    some fixes

--- src/components/ApartmentList.js
+++ src/components/ApartmentList.js
@@ -1,9 +1,17 @@
-const ApartmentList = ({ apartments }) => {
+import { useHistory } from 'react-router-dom'
 
+
+const ApartmentList = ({ apartments }) => {
+  const history = useHistory();
   return (
-    <div className="apartment-list">
+    <div className="apartment-list" >
+      
       {apartments.map((apartment) => (
-        <div className="apartment-preview" key={apartment._id}>
+        <div 
+        className="apartment-preview" 
+        key={apartment._id} 
+        onClick={() => history.push('/details/' + apartment._id)}
+        >
           <img src={apartment.gallery[0]} alt="" />
           <h3>{apartment.city}</h3>
           <p>{apartment.description}</p>
--- src/pages/ApartmentDetails.js
+++ src/pages/ApartmentDetails.js
@@ -2,16 +2,19 @@ import { useParams } from 'react-router-dom'
 import { useContext } from 'react'
 //import homes from '../components/homecomps/homestest.json';
 import { ApartmentContext } from '../contexts/ApartmentContextProvider'
-import ApartmentList from "../components/ApartmentList";
-
-export default function HouseDetails(props) {
-    const { city } = useParams();
+export default function ApartmentDetails(props) {
+    const { id } = useParams();
     const { apartments } = useContext(ApartmentContext)
+
+    const apartment = apartments.find(a => a.id == id)
     
+    console.log(id)
+    console.log(apartment)
+    console.log(apartments)
     return(
+        
         <div>
         <h1>hejsan</h1>
-        
         </div>
     )
 }   
