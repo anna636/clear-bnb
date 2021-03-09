33542b39c053c81b21170e17f77dfa6dba8611fe
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-03-09 11:21:29 +0100

    get something to show on details page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Nav from './components/Nav'
 import Home from './pages/Home.js'
 import Footer from './components/Footer'
+import HouseDetails from './pages/HouseDetails.js'
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Home />
+      <HouseDetails/>
       <Footer />
     </div>
   );
---
+++ src/pages/HouseDetails.js
@@ -0,0 +1,8 @@
+
+
+export default function HouseDetails(props){
+
+    return(
+        <h1> hejsan </h1>
+    )
+}
