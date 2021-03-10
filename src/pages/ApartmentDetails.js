9da531b99071793ae718ab32a6cfb40573110ae1
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-03-10 11:29:15 +0100

    Styling and fitting everything into detailsPage

--- src/pages/ApartmentDetails.js
+++ src/pages/ApartmentDetails.js
@@ -2,19 +2,25 @@ import { useParams } from 'react-router-dom'
 import { useContext } from 'react'
 //import homes from '../components/homecomps/homestest.json';
 import { ApartmentContext } from '../contexts/ApartmentContextProvider'
+import '../css/DetailPage.css'
+
 export default function ApartmentDetails(props) {
     const { id } = useParams();
     const { apartments } = useContext(ApartmentContext)
 
-    const apartment = apartments.find(a => a.id == id)
-    
-    console.log(id)
-    console.log(apartment)
-    console.log(apartments)
+    let apartment = apartments.find(el => el._id === id )
     return(
-        
-        <div>
-        <h1>hejsan</h1>
+        <div className="Details">
+        <h2>{apartment.city} </h2>
+        <p> {apartment.region}</p>
+        <img src ={apartment.gallery[0]} className="Detail-img" />
+        <h3 className="description-text">{apartment.description}</h3>
+        <p>Property has a capacity of: {apartment.maxGuests} guests</p>
+        <p>Price: {apartment.pricePerDay}kr / 24 hours</p>
+        <p>The landlord's name: {apartment.ownerId.fullName}</p>
+        <div align="right" className="btn-div-details">
+        <button className="next-btn-details">Next</button>
+        </div>
         </div>
     )
 }   
