187b6c331aa93e7c80104c28575557509a939480
Ermin <uzunic.e@gmail.com>
2021-03-10 08:39:54 +0100

    login popup option display

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,25 +1,40 @@
 import '../css/LoginModal.css'
+import Login from './Login'
+import Register from './Register'
+import {useState} from 'react'
 
-export default function LoginModal(props) {
+export default function LoginModal() {
+  const [login, setLogin] = useState(false);
+  const [register, setRegister] = useState(false);
 
   return (
     <div className="login-modal">
-      <div className="login-popup-div">
+      <div className="login-popup-div"
+           onClick={() => {
+             setRegister(!register);
+           }}      
+      >
         <p className="login-popup-content bli-medlem">Bli medlem</p>
       </div>
-      <div className="login-popup-div">
-      <p className="login-popup-content logga-in">Logga in</p>
+      <div className="login-popup-div"
+           onClick={() => {
+             setLogin(!login);
+           }}
+      >
+        <p className="login-popup-content logga-in">Logga in</p>
       </div>
       <hr className="login-popup-hr"/>
       <div className="login-popup-div">
-      <p className="login-popup-content bli-värd-boende">Bli värd för ditt boende</p>
+        <p className="login-popup-content bli-värd-boende">Bli värd för ditt boende</p>
       </div>
       <div className="login-popup-div">
-      <p className="login-popup-content bli-värd-upplevelse">Bli värd för en upplevelse</p>
+        <p className="login-popup-content bli-värd-upplevelse">Bli värd för en upplevelse</p>
       </div>
       <div className="login-popup-div">
-      <p className="login-popup-content hjälp">Hjälp</p>
+        <p className="login-popup-content hjälp">Hjälp</p>
       </div>
+      {register && <Register />}
+      {login && <Login />}
     </div>
   )
 }
