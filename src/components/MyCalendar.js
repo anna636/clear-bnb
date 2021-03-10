0382a2c5fa7f0cd939f8dc81f43aac2e7d2a09d8
Denise <denisebradley92@hotmail.com>
2021-03-10 19:13:29 +0100

    Select date range works

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -4,11 +4,10 @@ import { useHistory } from 'react-router-dom'
 
 export default function ReactCalendar({ userSearch }) {
   const history = useHistory()
-  const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState() // date is array of 2 dates
 
   const onChange = newDate => {
     setDate(newDate)
-    console.log(newDate)
   }
 
   function next() {
@@ -17,8 +16,15 @@ export default function ReactCalendar({ userSearch }) {
 
   return (
     <div>
-      <Calendar minDate={new Date()} onChange={onChange} value={date} selectRange={true}  />
+      <Calendar minDate={new Date()} onChange={onChange} value={date} selectRange={true} />
       <button onClick={next}>Next</button>
+      <div>
+        {date && date.map((d) => (
+          <div key={d.toString()}>
+            <h3>{ d.toString() }</h3>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
