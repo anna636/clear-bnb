f21be9ce7878f5e81e94bb2d56897bd4a9aaf354
Alexander S <70532114+autumnjava@users.noreply.github.com>
2021-03-12 15:01:41 +0100

    refactoring
    Co-Authored-By: JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>

--- backend/auth.js
+++ backend/auth.js
@@ -1,103 +1,112 @@
-/*const crypto = require('crypto');
+const app = global.app;
+const express = require("express");
+
 const session = require('express-session');
 const connectMongo = require('connect-mongo')(session);
-import server from ('./server.js')
 
-// Mongoose model
-const Models = require('./models');
-const User = Models.User;
-const secret = 'w124##3£1jnwjk12j4!"¤141';
+const crypto = require('crypto');
+const models = require("./models/models.js");
 
+const User = models.users;
+const secret = 'H!#45i12ip154?31!+';
 
-// session middleware
-app.use(session({
-  secret: 'very secret one', // choose your own...
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false },
-  store: new connectMongo({mongooseConnection: mongoose.connection})
-}));
+app.use(express.json());
 
-// Route to register a user
-app.post('/api/register', async (req, res) => {
-  // Encrypt password
-  const hash = crypto.createHmac('sha256', secret)
-    .update(req.body.password).digest('hex');
-  // Create new user
-  let user = new User({...req.body, password: hash});
-  // NOTE: This system is unsafe since you can 
-  // choose your own role on registration!
-  await user.save();
-  res.json({success: true});
-});
+function authLogic() {
+  app.use(session({
+    secret: 'very secret one', // choose your own...
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+    store: new connectMongo({ mongooseConnection: mongoose.connection })
+  }));
 
-// Login
-app.post('/api/login', async (req, res) => {
-  // note: req.session is unique per user/browser
-  if(req.session.user){
-    res.json({error: 'Someone is already logged in'});
-    return;
-  }
-  // Encrypt password
-  const hash = crypto.createHmac('sha256', secret)
-    .update(req.body.password).digest('hex');
-  // Search for user
-  let user = await User.findOne({username: req.body.username, password: hash});
-  if(user){
-    // succesful login, save the user to the session object
-    req.session.user = user;
-    res.json({success: 'Logged in'});
-  }
-  else {
-    res.json({error: 'No match.'});
-  }
-});
+  // Route to register a user
+  app.post('/api/register', async (req, res) => {
+    // Encrypt password
+    const hash = crypto.createHmac('sha256', secret)
+      .update(req.body.password).digest('hex');
+    // Create new user
+    let user = new User({ ...req.body, password: hash });
+    // NOTE: This system is unsafe since you can 
+    // choose your own role on registration!
+    try {
+      await user.save();
+    } catch (err) {
+      res.json('error!')
+      return;
+    }
 
-// Logout
-app.delete('/api/logout', (req, res) => {
-  if(req.session.user){
-    delete req.session.user;
-    res.json({success: 'Logged out'});
-  }
-  else {
-    res.json({error: 'Was not logged in'});
-  }
-});
+    res.json({ success: true });
+  });
 
-// Check if logged in
-app.get('/api/whoami', (req, res) => {
-  if(req.session.user){
-    let user = {...req.session.user};
-    delete user.password; // remove password in answer
-    res.json(user);
-  }
-  else {
-    res.json({error: 'Not logged in'});
-  }
-});
+  // Login
+  app.post('/api/login', async (req, res) => {
+    // note: req.session is unique per user/browser
+    if (req.session.user) {
+      res.json({ error: 'Someone is already logged in' });
+      return;
+    }
+    // Encrypt password
+    const hash = crypto.createHmac('sha256', secret)
+      .update(req.body.password).digest('hex');
+    // Search for user
+    let user = await User.findOne({ username: req.body.username, password: hash });
+    if (user) {
+      // succesful login, save the user to the session object
+      req.session.user = user;
+      res.json({ success: 'Logged in' });
+    }
+    else {
+      res.json({ error: 'No match.' });
+    }
+  });
 
-// Example rotues and roles - ACL Access Control List
-app.get('/api/bird-admin-page', (req, res) => {
-  if(req.session.user && req.session.user.role === 'bird admin'){
-    res.json({secret: 'The secret data, only for bird admins.'})
-  }
-  else {
-    res.json({error: 'No rights to see this route!'});
-  }
-});
+  // Logout
+  app.delete('/api/logout', (req, res) => {
+    if (req.session.user) {
+      delete req.session.user;
+      res.json({ success: 'Logged out' });
+    }
+    else {
+      res.json({ error: 'Was not logged in' });
+    }
+  });
 
-// Example rotues and roles - ACL Access Control List
-app.get('/api/cat-admin-page', (req, res) => {
-  if(req.session.user && req.session.user.role === 'cat admin'){
-    res.json({secret: 'The secret data, only for cat admins.'})
-  }
-  else {
-    res.json({error: 'No rights to see this route!'});
-  }
-});
+  // Check if logged in
+  app.get('/api/whoami', (req, res) => {
+    if (req.session.user) {
+      let user = { ...req.session.user };
+      delete user.password; // remove password in answer
+      res.json(user);
+    }
+    else {
+      res.json({ error: 'Not logged in' });
+    }
+  });
 
-// Start the web server
-app.listen(3001, () => console.log('Listening on port 3000'));
+  // Example rotues and roles - ACL Access Control List
+  app.get('/api/bird-admin-page', (req, res) => {
+    if (req.session.user && req.session.user.role === 'bird admin') {
+      res.json({ secret: 'The secret data, only for bird admins.' })
+    }
+    else {
+      res.json({ error: 'No rights to see this route!' });
+    }
+  });
 
-*/
+  // Example rotues and roles - ACL Access Control List
+  app.get('/api/cat-admin-page', (req, res) => {
+    if (req.session.user && req.session.user.role === 'cat admin') {
+      res.json({ secret: 'The secret data, only for cat admins.' })
+    }
+    else {
+      res.json({ error: 'No rights to see this route!' });
+    }
+  });
 
+}
+
+module.exports = {
+  authLogic: authLogic()
+}
