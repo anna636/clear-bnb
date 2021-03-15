5f873ff85685092d2da02d5a4b42faf510f5cb76
Denise <denisebradley92@hotmail.com>
2021-03-15 11:08:37 +0100

    css to apartment list

--- src/components/ApartmentList.js
+++ src/components/ApartmentList.js
@@ -6,9 +6,11 @@ const ApartmentList = ({ apartments }) => {
     <div>
       {apartments.map((apartment) => (
         <div className="apartment-preview" key={apartment._id}>
-          <img src={apartment.gallery[0]} alt="" className="apartment-img" />
+          <div className="img-container">
+            <img src={apartment.gallery[0]} alt="" className="apartment-img" />
+          </div>
           <div className="apartment-description">
-          <h3>{apartment.city}</h3>
+            <h3>{apartment.city}</h3>
             <p>{apartment.description}</p>
           </div>
         </div>
