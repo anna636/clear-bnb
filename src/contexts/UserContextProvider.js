3a701174d2579be534d49f6c56f167f42b25720a
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-03-15 15:38:06 +0100

    Fixed login
    Co-Authored-By: JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
    Co-Authored-By: Alexander S. <70532114+autumnjava@users.noreply.github.com>

--- src/contexts/UserContextProvider.js
+++ src/contexts/UserContextProvider.js
@@ -1,25 +1,20 @@
 import { createContext, useState, useEffect } from "react";
 
-export const UserContext=createContext()
+export const UserContext = createContext();
 
 export default function UserContextProvider(props) {
-  
-  const [users, setUsers] = useState([])
-  const [newUser, setNewUser] = useState()
-  
-    const fetchUsers = async () => {
-      let res = await fetch("/rest/users");
-      res = await res.json();
-      setUsers(res);
-    };
-
-    useEffect(() => {
-      
-      fetchUsers();
-      
-    }, []);
+  const [users, setUsers] = useState([]);
 
 
+  const fetchUsers = async () => {
+    let res = await fetch("/rest/users");
+    res = await res.json();
+    setUsers(res);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   const addUser = async (user) => {
     let res = await fetch("/api/register", {
@@ -31,21 +26,27 @@ export default function UserContextProvider(props) {
     res = await res.json();
     user.id = res.id;
 
-    console.log('The user object is being saved', res);
+    console.log("The user object is being saved", res);
     setUsers([...users, user]);
-      
-  }
- 
+  };
+
+  const login = async (user) => {
+    let res = await fetch("/api/login", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(user),
+    });
+
+    res = await res.json();
+  };
+
   const values = {
     users,
     addUser,
-    setNewUser
+    login
   };
 
   return (
-    <UserContext.Provider value={values}>
-      {props.children}
-    </UserContext.Provider>
+    <UserContext.Provider value={values}>{props.children}</UserContext.Provider>
   );
-  
 }
