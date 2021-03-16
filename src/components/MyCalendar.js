ebc81d59c010c8e3746b462678161b9fd0c3eb24
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-03-16 09:58:04 +0100

    Made confirmation page with info

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -1,26 +1,26 @@
 import "../css/Calendar.css";
-import React, { useState, useContext } from 'react'
-import { BookingContext } from '../contexts/BookingContextProvider'
-import Calendar from 'react-calendar'  // npm i react-calendar
-import { useHistory } from 'react-router-dom'
-const moment = require("moment");  // npm i moment
-
+import React, { useState, useContext } from "react";
+import { BookingContext } from "../contexts/BookingContextProvider";
+import Calendar from "react-calendar"; // npm i react-calendar
+import { useHistory } from "react-router-dom";
+const moment = require("moment"); // npm i moment
 
 export default function ReactCalendar({ userSearch }) {
-  const history = useHistory()
-  const [dates, setDates] = useState() // dates is array of 2 dates picked
-  const { addCalendarDates } = useContext(BookingContext)
+  const history = useHistory();
+  const [dates, setDates] = useState(); // dates is array of 2 dates picked
+  const { addCalendarDates, addGuests } = useContext(BookingContext);
 
-  const onChange = newDate => {
-    setDates(newDate)
-  }
+  const onChange = (newDate) => {
+    setDates(newDate);
+  };
 
   function next() {
-    const getDatesArray = getDates(dates[0], dates[1])
-    console.log(getDatesArray)
-    addCalendarDates(getDatesArray)
+    const getDatesArray = getDates(dates[0], dates[1]);
+    console.log(getDatesArray);
+    addGuests(1);
+    addCalendarDates(getDatesArray);
 
-    history.push('/search/' + userSearch)
+    history.push("/search/" + userSearch);
   }
 
   //Function to get array of dates between 2 dates
@@ -43,7 +43,9 @@ export default function ReactCalendar({ userSearch }) {
         value={dates}
         selectRange={true}
       />
-      <button className="calendarNext" onClick={next}>Next</button>
+      <button className="calendarNext" onClick={next}>
+        Next
+      </button>
       <div>
         <p>Just a check that it works: </p>
         {dates &&
