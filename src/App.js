6fe25fd72a4c6316a7ab43ae12c43ccdf4292f92
Ermin <uzunic.e@gmail.com>
2021-03-16 16:25:31 +0100

    dependecie needed

--- src/App.js
+++ src/App.js
@@ -21,52 +21,52 @@ function App() {
   return (
     <div className="App">
       <BookingContextProvider>
-   <HousingContextProvider>
-        <ApartmentContextProvider>
-          <UserContextProvider>
-            <Router>
-              <header>
-                <Nav />
-              </header>
-              <main>
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route
-                    exact
-                    path="/search/:city"
-                    component={ApartmentSearch}
-                  />
-                  <Route
-                    exact
-                    path="/plusminus/:id"
-                    component={PlusMinusGuests}
-                  />
-                  <Route
-                    exact
-                    path="/details/:id"
-                    component={ApartmentDetails}
-                  />
-                  <Route exact path="/checkin/:id" component={Checkin} />
+        <HousingContextProvider>
+          <ApartmentContextProvider>
+            <UserContextProvider>
+              <Router>
+                <header>
+                  <Nav />
+                </header>
+                <main>
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route
+                      exact
+                      path="/search/:city"
+                      component={ApartmentSearch}
+                    />
+                    <Route
+                      exact
+                      path="/plusminus/:id"
+                      component={PlusMinusGuests}
+                    />
+                    <Route
+                      exact
+                      path="/details/:id"
+                      component={ApartmentDetails}
+                    />
+                    <Route exact path="/checkin/:id" component={Checkin} />
 
-                  <Route
-                    exact
-                    path="/confirmation/:id"
-                    component={Confirmation}
-                  />
-  
-                  <Route exact path="/housing-listing" component={Housing} />
+                    <Route
+                      exact
+                      path="/confirmation/:id"
+                      component={Confirmation}
+                    />
+    
+                    <Route exact path="/housing-listing" component={Housing} />
 
-                  <Route path="*" component={page404} />
-                </Switch>
-              </main>
+                    <Route path="*" component={page404} />
+                  </Switch>
+                </main>
 
-              <footer>
-                <Footer />
-              </footer>
-            </Router>
-          </UserContextProvider>
-        </ApartmentContextProvider>
-    </HousingContextProvider>
+                <footer>
+                  <Footer />
+                </footer>
+              </Router>
+            </UserContextProvider>
+          </ApartmentContextProvider>
+        </HousingContextProvider>
       </BookingContextProvider>
     </div>
   );
