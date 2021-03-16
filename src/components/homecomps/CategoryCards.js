224bf2b170fbe89a73c9857dd3b36b74a34c223f
Ermin <uzunic.e@gmail.com>
2021-03-16 21:38:05 +0100

    categories styling changes

--- src/components/homecomps/CategoryCards.js
+++ src/components/homecomps/CategoryCards.js
@@ -6,34 +6,33 @@ export default function CategoryCards() {
       <h1>Types of apartments</h1>
       <div className="categories">
         <div className="house-category-card category-card">
-          <h2>Houses</h2>
           <img
             src="https://i.imgur.com/aT5ei7L.jpg"
             className="category-img"
             alt=""
           />
-          <div className="category-description">{/* <h3></h3> */}</div>
+          <h2>Houses</h2>
+          {/* <div className="category-description"><h3></h3></div> */}
         </div>
 
         <div className="apartment-category-card category-card">
-          <h2>Flats</h2>
           <img
             src="https://i.imgur.com/14aRNot.jpg"
             className="category-img"
             alt=""
           />
-
-          <div className="category-description">{/* <h3></h3> */}</div>
+          <h2>Flats</h2>
+          {/* <div className="category-description"><h3></h3></div> */}
         </div>
 
         <div className="cabin-category-card category-card">
-          <h2>Cottages</h2>
           <img
             src="https://i.imgur.com/Z0lwKtq.jpg"
             alt=""
             className="category-img"
           />
-          <div className="category-description">{/* <h3></h3> */}</div>
+          <h2>Cottages</h2>
+          {/* <div className="category-description"><h3></h3></div> */}
         </div>
       </div>
     </div>
