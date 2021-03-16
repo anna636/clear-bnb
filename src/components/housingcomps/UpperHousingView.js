f900126c9b7e8f83843f14165e856a7532ab923d
Ermin <uzunic.e@gmail.com>
2021-03-16 10:22:04 +0100

    filter buttons rendering input fields

--- src/components/housingcomps/UpperHousingView.js
+++ src/components/housingcomps/UpperHousingView.js
@@ -1,10 +1,57 @@
 import '../../css/UpperHousingView.css'
 import {HousingContext} from '../../contexts/HousingContextProvider'
-import { useContext } from 'react'
+import {useContext, useState} from 'react'
 
-export default function UpperHousingView(props) {
+export default function UpperHousingView() {
+  const [showRegionInput, setRegionDisplay] = useState(false);
+  const [showCityInput, setCityDisplay] = useState(false);
+  const [showPriceInput, setPriceDisplay] = useState(false);
+  const [showTypeInput, setTypeDisplay] = useState(false);
   const {apartments} = useContext(HousingContext);
+  let highestPrice = highestPriced();
+  let lowestPrice = lowestPriced();
 
+  function highestPriced() {
+    let res = apartments.map(apartment => apartment.pricePerDay);
+    return (Math.max(...res));
+  };
+
+  function lowestPriced() {
+    let res = apartments.map(apartment => apartment.pricePerDay);
+    return (Math.min(...res));
+  };
+
+  /* Subcomponents
+     Region input
+     City input
+     Price input
+     Type input
+  */
+
+  const regionInput = (
+    <div className="regionInput">
+      <input placeholder="Search region..."></input>
+    </div>
+  );
+  
+  const cityInput = (
+    <div className="cityInput">
+      <input placeholder="Search city..."></input>
+    </div>
+  );
+
+  const priceInput = (
+    <div className="cityInput">
+      <input type="range" min={lowestPrice} max={highestPrice}></input>
+    </div>
+  );
+
+  const typeInput = (
+    <div className="typeInput">
+      <input placeholder="Search type..."></input>
+    </div>
+  );
+  
   return (
     <div className="upper-housing-container">
       <div>
@@ -12,10 +59,30 @@ export default function UpperHousingView(props) {
         <h1 className="upper-housing-view-title">Housings</h1>
       </div>
       <div>
-        <button className="upper-housing-view-btn">Region</button>
-        <button className="upper-housing-view-btn">City</button>
-        <button className="upper-housing-view-btn">Price</button>
-        <button className="upper-housing-view-btn">Type</button>
+        <button
+          onClick={() => {setRegionDisplay(!showRegionInput)}}
+          className="upper-housing-view-btn">
+          Region
+        </button>
+        <button
+          onClick={() => {setCityDisplay(!showCityInput)}}
+          className="upper-housing-view-btn">
+          City
+        </button>
+        <button
+          onClick={() => {setPriceDisplay(!showPriceInput)}} 
+          className="upper-housing-view-btn">
+          Price
+        </button>
+        <button
+          onClick={() => {setTypeDisplay(!showTypeInput)}} 
+          className="upper-housing-view-btn">
+          Type
+        </button>
+        {showRegionInput && regionInput}
+        {showCityInput && cityInput}
+        {showPriceInput && priceInput}
+        {showTypeInput && typeInput}
       </div>
     </div>
   )
