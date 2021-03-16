53b989366dc5bb5b6351424ccea47cf5377bd0e4
Ermin <uzunic.e@gmail.com>
2021-03-16 21:56:25 +0100

    location heading styling and database fetching object

--- src/components/homecomps/HomesHomepageDisplay.js
+++ src/components/homecomps/HomesHomepageDisplay.js
@@ -1,25 +1,26 @@
 import '../../css/HomesHomepageDisplay.css'
 import homes from './homestest.json'
+import {useContext} from 'react'
+import {HousingContext} from '../../contexts/HousingContextProvider'
 
 export default function HomesHomepageDisplay() {
+  const {apartments} = useContext(HousingContext);
+
   const card = home => (
     <div className="home-comp-wrapper" key={Math.random()}>
       <div className="home-comp-img">
-        <img src={home.img}
+        <img src={home.gallery[0]}
           alt=""
           className="home-list-img"
         />
       </div>
       <div className="home-attr">
-        <h6>{home.rating}</h6>
-        <h6>{home.owner}</h6>
+        {/* <h6>{home.rating}</h6> */}
+        <h6>{home.ownerId.fullName}</h6>
         <i className="fas fa-user-circle"></i>
-
-        <h4>{home.type}</h4>
-        <h4> · </h4>
+        {/* <h4>{home.type}</h4> */}
         <h4>{home.city}</h4>
-        <h6>{home.subs}</h6>
-        <h5>{home.price}</h5>
+        <h5>{home.pricePerDay}</h5>
       </div>
     </div>
   );
@@ -28,8 +29,10 @@ export default function HomesHomepageDisplay() {
     <div className="homes-display">
       <h1>Discover apartments</h1>
       <div className="home-elements">
-        {homes.map(home => {
-          return card(home);
+        {apartments.map((home, i) => {
+          while(i < 8) {
+            return card(home);  
+          }
         })}
       </div>
 
