ca848dcb0ed038cb793d7848df864d0b06e7d99e
Alexander S <70532114+autumnjava@users.noreply.github.com>
2021-03-16 13:47:22 +0100

    fixy fix

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,40 +1,45 @@
 import '../css/LoginModal.css'
 import Login from './Login'
 import Register from './Register'
-import {useState} from 'react'
+import { useState, useContext } from 'react'
+import { UserContext } from '../contexts/UserContextProvider'
 
 export default function LoginModal() {
   const [login, setLogin] = useState(false);
   const [register, setRegister] = useState(false);
+  const { getCurrentUser } = useContext(UserContext)
 
   return (
     <div className="login-modal">
-      <div className="login-popup-div"
-           onClick={() => {
-             setRegister(!register);
-           }}      
-      >
-        <p className="login-popup-content bli-medlem">Bli medlem</p>
-      </div>
-      <div className="login-popup-div"
-           onClick={() => {
-             setLogin(!login);
-           }}
-      >
-        <p className="login-popup-content logga-in">Logga in</p>
-      </div>
-      <hr className="login-popup-hr"/>
-      <div className="login-popup-div">
-        <p className="login-popup-content bli-värd-boende">Bli värd för ditt boende</p>
-      </div>
-      <div className="login-popup-div">
-        <p className="login-popup-content bli-värd-upplevelse">Bli värd för en upplevelse</p>
-      </div>
-      <div className="login-popup-div">
-        <p className="login-popup-content hjälp">Hjälp</p>
-      </div>
+      { !getCurrentUser() &&
+        <>
+          <div className="login-popup-div"
+            onClick={() => {
+              setRegister(!register);
+            }}
+          >
+            <p className="login-popup-content bli-medlem">Register</p>
+          </div>
+          <div className="login-popup-div"
+            onClick={() => {
+              setLogin(!login);
+            }}
+          >
+            <p className="login-popup-content logga-in">Login</p>
+          </div>
+        </>
+      }
+
+      {getCurrentUser &&
+        <>
+          <div className="login-popup-div">
+            <p className="login-popup-content bli-värd-boende">Rent out</p>
+          </div>
+        </>
+      }
       {register && <Register />}
       {login && <Login />}
+
     </div>
   )
 }
