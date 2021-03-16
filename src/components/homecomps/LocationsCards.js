3712615c4fbdf3bc78a71100ce2cb55e96d9cac6
Alexander S <70532114+autumnjava@users.noreply.github.com>
2021-03-16 09:56:19 +0100

    random key

--- src/components/homecomps/LocationsCards.js
+++ src/components/homecomps/LocationsCards.js
@@ -2,12 +2,12 @@ import '../../css/LocationCards.css'
 
 export default function LocationCards() {
   let locations = ['Malmö', 'Stockholm', 'Kristianstad', 'Växjö', 'Jönköping', 'Lund'];
-  
+
   let card = location => (
-    <div className="card">
+    <div className="card" key={Math.random()}>
       <img src="https://i.imgur.com/yEpIfji.jpg"
-           alt=""
-           className="location-img"
+        alt=""
+        className="location-img"
       />
       <h3 className="card-title">{location}</h3>
     </div>
