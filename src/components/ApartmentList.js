b3f475060774bf686e3390081e2a95d9f53ce341
Alexander S <70532114+autumnjava@users.noreply.github.com>
2021-03-16 09:55:54 +0100

    fix css syntax

--- src/components/ApartmentList.js
+++ src/components/ApartmentList.js
@@ -8,17 +8,17 @@ const ApartmentList = ({ apartments }) => {
     <div>
       {apartments.map((apartment) => (
         <div
-        onClick={() => history.push('/details/' + apartment._id)}
+          onClick={() => history.push('/details/' + apartment._id)}
           className="apartment-preview" key={apartment._id}>
           <div className="img-container">
             <img src={apartment.gallery[0]} alt="" className="apartment-img" />
           </div>
           <div className="apartment-description">
-            <h3>{apartment.title} | { apartment.city }</h3>
+            <h3>{apartment.title} | {apartment.city}</h3>
             <p>{apartment.description}</p>
             {apartment.amenities.map((amenity) => (
-              <div className="amenities" key={amenity.id} style={{ fontSize: "15px" }, { color: "rgb(82, 82, 82)"}}>
-                <p> -{ amenity.name } </p>
+              <div className="amenities" key={amenity.id} style={{ fontSize: "15px", color: "rgb(82, 82, 82" }}>
+                <p> -{amenity.name} </p>
               </div>
             ))}
           </div>
