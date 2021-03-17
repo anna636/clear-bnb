d41bfb04bdd86b5ae3ebc9a3364707e7aa776002
Ermin <uzunic.e@gmail.com>
2021-03-17 03:25:06 +0100

    homepage fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,18 @@ export default function Home() {
     <div className="home">
       <Banner />
       <LocationCards />
+      <div className="homepage-vertical-line" style={verticalLine}/>
       <CategoryCards />
+      <div className="homepage-vertical-line" style={verticalLine}/>
       <HomesHomepageDisplay />
+      <div className="homepage-vertical-line" style={verticalLine}/>
     </div>
   );
+}
+
+const verticalLine = {
+  width: '90%',
+  height: '1px',
+  margin: '50px auto',
+  background: 'rgba(0, 0, 0, 0.1)'
 }
