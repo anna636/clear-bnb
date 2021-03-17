afe75cb63ed53d253de6e80fbedae4f07096488b
Ermin <uzunic.e@gmail.com>
2021-03-17 03:04:17 +0100

    homepage residence-rendering from database

--- src/components/homecomps/HomesHomepageDisplay.js
+++ src/components/homecomps/HomesHomepageDisplay.js
@@ -2,6 +2,7 @@ import '../../css/HomesHomepageDisplay.css'
 import homes from './homestest.json'
 import {useContext} from 'react'
 import {HousingContext} from '../../contexts/HousingContextProvider'
+import {Link} from 'react-router-dom'
 
 export default function HomesHomepageDisplay() {
   const {apartments} = useContext(HousingContext);
@@ -15,19 +16,30 @@ export default function HomesHomepageDisplay() {
         />
       </div>
       <div className="home-attr">
-        {/* <h6>{home.rating}</h6> */}
-        <h6>{home.ownerId.fullName}</h6>
-        <i className="fas fa-user-circle"></i>
-        {/* <h4>{home.type}</h4> */}
-        <h4>{home.city}</h4>
-        <h5>{home.pricePerDay}</h5>
+        <div className="housing-location-div">
+          <h2>{home.city}</h2>
+          <h4 className="region"><i class="fas fa-map-marker-alt"></i>{home.region}</h4>
+        </div>
+        <div className="renter-div">
+          <h5>{home.ownerId.fullName}</h5>
+          <i className="fas fa-user-circle"></i>
+        </div>
+      </div>
+      <div className="housing-price-div">
+        <p className="price-title inline">Price/night: </p>
+        <h6 className="inline">{home.pricePerDay} €</h6>
       </div>
     </div>
   );
 
   return (
     <div className="homes-display">
-      <h1>Discover apartments</h1>
+      <div className="homes-top-container">
+        <h1>Discover apartments</h1>
+        <Link to="/housing-listing">
+          <i className="far fa-arrow-alt-circle-right"></i>
+        </Link>
+      </div>
       <div className="home-elements">
         {apartments.map((home, i) => {
           while(i < 8) {
