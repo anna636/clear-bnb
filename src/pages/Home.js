d93b1db12c9539312166f85ffbb676e7dc7d9499
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-03-17 13:25:31 +0100

    Deleted categories of apartments
    Co-Authored-By: Denise <72066356+DeniiseB@users.noreply.github.com>
    Co-Authored-By: ErminUz <26276586+ErminUz@users.noreply.github.com>
    Co-Authored-By: JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
    Co-Authored-By: Alexander S. <70532114+autumnjava@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,12 @@
-import Banner from '../components/homecomps/Banner'
-import CategoryCards from '../components/homecomps/CategoryCards'
-import HomesHomepageDisplay from '../components/homecomps/HomesHomepageDisplay'
-import LocationCards from '../components/homecomps/LocationsCards'
-
-
+import Banner from "../components/homecomps/Banner";
+import HomesHomepageDisplay from "../components/homecomps/HomesHomepageDisplay";
+import LocationCards from "../components/homecomps/LocationsCards";
 
 export default function Home() {
   return (
     <div className="home">
       <Banner />
       <LocationCards />
-      <CategoryCards />
       <HomesHomepageDisplay />
     </div>
   );
