4734b70b7398629b0af536a04680f0c4593bd5be
Denise <denisebradley92@hotmail.com>
2021-03-18 18:24:00 +0100

    css fixes

--- src/pages/ApartmentDetails.js
+++ src/pages/ApartmentDetails.js
@@ -24,14 +24,19 @@ export default function ApartmentDetails(props) {
         <p> {apartment.region}</p>
         <img src={apartment.gallery[0]} className="Detail-img" />
         <h3 className="description-text">{apartment.description}</h3>
+        <div className="details-icons">
+          {apartment.amenities.map((amenity) => (
+            <div className="amenities" key={amenity._id} style={{ fontSize: "15px" }, { color: "black" }}>
+              <i className={amenity.icon} /> <span>{amenity.name} </span>
+            </div>
+          ))}
+        </div>
         <p>Property has a capacity of: {apartment.maxGuests} guests</p>
         <p>Price: {apartment.pricePerDay}kr / 24 hours</p>
-      <p>The landlord's name: {apartment.ownerId.fullName}</p>
-      <div className="details-icons">
-      </div>
-      <div className="details-calendar">
-        <DetailsCalendar apartmentId={apartment._id} />
-      </div>
+        <p>The landlord's name: {apartment.ownerId.fullName}</p>
+        <div className="details-calendar">
+          <DetailsCalendar apartmentId={apartment._id} />
+        </div>
         <div align="right" className="btn-div-details">
           <button className="next-btn-details" onClick={goToCheckIn}>Next</button>
         </div>
