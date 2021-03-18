ed8caf53bdd661587ebf63bc810c5d2e6f6fe076
Denise <denisebradley92@hotmail.com>
2021-03-18 15:46:18 +0100

    important fix to MyCalendar

--- src/components/DetailsCalendar.js
+++ src/components/DetailsCalendar.js
@@ -10,6 +10,7 @@ export default function ReactCalendar({ userSearch }) {
   const [dates, setDates] = useState(); // dates is array of 2 dates picked
   const { addCalendarDates, addGuests } = useContext(BookingContext);
 
+
   const onChange = (newDate) => {
     setDates(newDate);
   };
@@ -23,11 +24,6 @@ export default function ReactCalendar({ userSearch }) {
   }
 
 
-  function skip() {
-    addCalendarDates([])
-    history.push("/search/" + userSearch);
-  }
-
   //Function to get array of dates between 2 dates
   function getDates(startDate, stopDate) {
     var dateArray = [];
@@ -37,7 +33,6 @@ export default function ReactCalendar({ userSearch }) {
       dateArray.push(moment(currentDate).format("YYYY-MM-DD"));
       currentDate = moment(currentDate).add(1, "days");
     }
-    console.log('getDates: ', ...dateArray)
     return dateArray;
   }
 
@@ -50,7 +45,6 @@ export default function ReactCalendar({ userSearch }) {
         selectRange={true}
       />
       <button className="calendarNext" disabled={!dates} onClick={next}>Next</button>
-      <button className="calendarSkip" onClick={skip}>Skip</button>
     </div>
   );
 }
