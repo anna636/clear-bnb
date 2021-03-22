4351a787344b677c0f0aa19a550e4f5617a55e1b
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-03-22 15:15:27 +0100

    Fixed bookings filter by date

---
+++ src/pages/MyApartments.js
@@ -0,0 +1,72 @@
+import '../css/MyApartments.css'
+import { ApartmentContext } from "../contexts/ApartmentContextProvider";
+import { BookingContext } from "../contexts/BookingContextProvider";
+import { UserContext } from "../contexts/UserContextProvider";
+import { useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+import { useContext, useState } from "react";
+
+export default function MyApartments() {
+
+
+  
+  const { apartments } = useContext(ApartmentContext);
+  const { currentUser} = useContext(UserContext);
+  
+  const myApartments = apartments.filter(function (apartment) {
+    return apartment.ownerId._id === currentUser._id;
+  });
+
+  const history = useHistory();
+  const { id } = useParams();
+
+
+  return (
+    <>
+      {apartments && !myApartments.length && (
+        <div className="noApartmentsFound">
+          <h1>You do not have any apartment for rent</h1>
+          <p>Would you like to post a new apartment?</p>
+          <button
+            className="rentOutApartmentButton"
+            onClick={() => history.push("/rest/postNewApartment")}
+          >
+            Yes let's begin!
+          </button>
+        </div>
+      )}
+
+      {Boolean(apartments && currentUser && myApartments.length) && (
+        <div className="apartmentsWrapper">
+          <h1 className="apartmentsHeader">Apartments for rent</h1>
+
+          <div className="apartmentsComp">
+            {myApartments.map((apartment) => (
+              <div
+                className="apartmentItem"
+                onClick={() => history.push("/details/" + apartment._id)}
+              >
+                <img className="rentOutImage" src={apartment.gallery[0]} />
+
+                <div className="apartmentInfoRent">
+                  <p>Dates: </p>
+                  <p>From {apartment.availableDates.availableStartDate}</p>
+                  <p>To {apartment.availableDates.availableEndDate}</p>
+                  <p>City: {apartment.city}</p>
+                  <p>
+                    Amount of guests allowed: {apartment.maxGuests}{" "}
+                    {apartment.maxGuests > 1 ? "people" : "person"}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+
+  
+    </>
+  );
+}
+
--- src/pages/MyBookings.js
+++ src/pages/MyBookings.js
@@ -1,4 +1,4 @@
-import '../css/MyBookings.css'
+import "../css/MyBookings.css";
 import { ApartmentContext } from "../contexts/ApartmentContextProvider";
 import { BookingContext } from "../contexts/BookingContextProvider";
 import { UserContext } from "../contexts/UserContextProvider";
@@ -8,64 +8,153 @@ import { useParams } from "react-router-dom";
 import { useContext, useState } from "react";
 
 export default function MyBookings() {
-
-
-  const { apartments } = useContext(ApartmentContext);
-   const { currentUser, users } = useContext(UserContext);
+  const { currentUser, users } = useContext(UserContext);
   const { bookings } = useContext(BookingContext);
-  const myBookings = bookings.filter(function(booking){
-  return booking.userId._id===currentUser._id;
-}); 
+  const myBookings = bookings.filter(function (booking) {
+    return booking.userId._id === currentUser._id;
+  });
 
-    const history = useHistory();
-    const { id } = useParams();
+  const bookingHistory = []; //För bokningar som var förut
+
+  const history = useHistory();
+  const { id } = useParams();
 
   function getOwner(ownerId) {
-    const owner = users.find(user => user._id === ownerId)
-    return owner
+    const owner = users.find((user) => user._id === ownerId);
+    return owner;
   }
 
+  //Om datum är mindre en den som är idag pusha booking till hisotry array och ta bort det från myBookings array
+  function getDate() {
+    myBookings.map((booking) => {
+      console.log(booking);
+
+      const todayDate = Date.parse(new Date());
+      const bookingDate = Date.parse(booking.endDate);
+
+      if (bookingDate < todayDate && !bookingHistory.includes(booking)) {
+        bookingHistory.push(booking);
+        myBookings.splice(myBookings.indexOf(booking), 1);
+      }
+    });
+  }
+
+  getDate(); //Kör metoden innan rendering
 
   return (
     <>
-      {bookings && currentUser && users && (
+      {Boolean(bookings && currentUser && users) && (
         <div className="bookingsWrapper">
           <h1>My bookings</h1>
           <div className="bookingsComp">
-            {myBookings.map((booking) => (
-              <div className="bookingItem">
-                <div className="apartmentImg">
-                  <img src={booking.apartmentId.gallery[0]} />
-                </div>
+            <div className="upcommingBookings">
+            
 
-                <div className="info">
-                  <div className="apartmentInfo">
-                    <p>City: {booking.apartmentId.city} </p>
-                    <p>
-                      Price per night: {booking.apartmentId.pricePerDay} eur
-                    </p>
-
-                    <p>
-                      Owner: {getOwner(booking.apartmentId.ownerId).fullName}{" "}
-                    </p>
-                  </div>
-                  <div className="dateInfo">
-                    <p>Check-in date: {booking.startDate}</p>
-                    <p>Check-out date: {booking.endDate}</p>
-                    <div className="buttonWrapperBooking">
-                    <button onClick={() => history.push("/details/" + booking.apartmentId._id)}>
-                        Show me this apartment
-                    </button>
+              {Boolean(myBookings.length) && (
+                <>
+                  {" "}
+                  {myBookings.map((booking) => (
+                    <div className="bookingItem">
+                      <div className="apartmentImg">
+                        <img src={booking.apartmentId.gallery[0]} />
                       </div>
-                  </div>
-                </div>
-              </div>
-            ))}
+
+                      <div className="info">
+                        <div className="apartmentInfo">
+                          <p>City: {booking.apartmentId.city} </p>
+                          <p>
+                            Price per night: {booking.apartmentId.pricePerDay}{" "}
+                            eur
+                          </p>
+
+                          <p>
+                            Owner:{" "}
+                            {getOwner(booking.apartmentId.ownerId).fullName}{" "}
+                          </p>
+                        </div>
+                        <div className="dateInfo">
+                          <p>Check-in date: {booking.startDate}</p>
+                          <p>Check-out date: {booking.endDate}</p>
+                          <div className="buttonWrapperBooking">
+                            <button
+                              onClick={() =>
+                                history.push(
+                                  "/details/" + booking.apartmentId._id
+                                )
+                              }
+                            >
+                              Show me this apartment
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}{" "}
+                </>
+              )}
+            </div>
+
+            {Boolean(bookingHistory.length) && (
+              <>
+                {" "}
+                <div className="historyBookings">
+                  <h3>History</h3>
+                  {bookingHistory.map((booking) => (
+                    <div className="bookingItem">
+                      <div className="apartmentImg">
+                        <img src={booking.apartmentId.gallery[0]} />
+                      </div>
+
+                      <div className="info">
+                        <div className="apartmentInfo">
+                          <p>City: {booking.apartmentId.city} </p>
+                          <p>
+                            Price per night: {booking.apartmentId.pricePerDay}{" "}
+                            eur
+                          </p>
+
+                          <p>
+                            Owner:{" "}
+                            {getOwner(booking.apartmentId.ownerId).fullName}{" "}
+                          </p>
+                        </div>
+                        <div className="dateInfo">
+                          <p>Check-in date: {booking.startDate}</p>
+                          <p>Check-out date: {booking.endDate}</p>
+                          <div className="buttonWrapperBooking">
+                            <button
+                              onClick={() =>
+                                history.push(
+                                  "/details/" + booking.apartmentId._id
+                                )
+                              }
+                            >
+                              Show me this apartment
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>{" "}
+              </>
+            )}
           </div>
+        </div>
+      )}
+
+
+      {Boolean(
+        bookings &&
+          currentUser &&
+          users &&
+          !myBookings.length &&
+          !bookingHistory.length
+      ) && (
+        <div className="bookingsWrapper noBookings">
+          <h1>You have no bookings</h1>
         </div>
       )}
     </>
   );
 }
-
-
