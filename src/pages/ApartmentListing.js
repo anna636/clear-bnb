a10a1f2c90583f9276933b5896efd1715fb86f50
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-03-23 11:19:06 +0100

    fixes

--- src/pages/ApartmentListing.js
+++ src/pages/ApartmentListing.js
@@ -1,8 +1,14 @@
+import { useContext } from 'react'
+import { ApartmentContext } from '../contexts/ApartmentContextProvider'
 import '../css/ApartmentListing.css'
 
+
 export default function ApartmentListing(){
-
-
+    const { apartments } = useContext(ApartmentContext)
+    const amenities = apartments.amenities;
+    console.log(apartments.amenities)
+    console.log(amenities)
+    
     return(
         <div>
             <form>
@@ -18,67 +24,67 @@ export default function ApartmentListing(){
                 <div class="checkboxes-div">
                 <div class="ameneties-div"><p>Amenities:</p></div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
-                        <span class="checkmark">Bathtub</span>
+                        <span class="checkmark"></span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
                         <span class="checkmark">Wifi</span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
                         <span class="checkmark">Dedicated Workspace</span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
                         <span class="checkmark">Essentials</span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
                         <span class="checkmark">Private Entrance</span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
                         <span class="checkmark">Breakfast</span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
                         <span class="checkmark">TV</span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
-                        <span class="checkmark">Air Conditioning</span>
+                        <span class="checkmark">Air Condition</span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
                         <span class="checkmark">Smoke Alarm</span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
                         <span class="checkmark">Kitchen</span>
                     </label>
                     </div>
                     <div class ="checkbox-wrapper">
-                    <label class="container">
+                    <label class="checkboxContainer">
                         <input type="checkbox" />
                         <span class="checkmark">CCTV</span>
                     </label>
