deabc3502b2455fb5ea77577476846c4e2a5faf9
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-03-24 13:47:27 +0100

    Merge branch 'develop' into feature-calendar-for-mobile

--- src/pages/ApartmentDetails.js
+++ src/pages/ApartmentDetails.js
@@ -4,53 +4,62 @@ import { useContext, useState } from 'react'
 import { ApartmentContext } from '../contexts/ApartmentContextProvider'
 import '../css/DetailPage.css'
 import Carousel from "react-bootstrap/Carousel";
+import DetailsCalendar from '../components/DetailsCalendar'
+import { BookingContext } from "../contexts/BookingContextProvider";
 
 export default function ApartmentDetails(props) {
-
+  const history = useHistory()
+  const { id } = useParams();
+  const { apartments } = useContext(ApartmentContext)
+  const { calendarDates } = useContext(BookingContext)
   
-  
-    const { id } = useParams();
-    const { apartments } = useContext(ApartmentContext)
-    const history=useHistory()
 
 
-    let apartment = apartments.find(el => el._id === id)
-    
-    function goToCheckIn() {
-      history.push("/checkin/" + apartment._id);
-    }
+  let apartment = apartments.find(el => el._id === id)
+
+  function goToCheckIn() {
+    history.push("/checkin/" + apartment._id);
+  }
 
   return (
-    <>
-      {Boolean(apartments && apartment) && (
+    <>{Boolean(apartments && apartment) && (
+      <>
+      <div className="Details">
+        <h2>{apartment.city} </h2>
+      <p> {apartment.region}</p>
+      <div className="gallery">
+        <Carousel fade>
+          {apartment.gallery.map((picture) => (
+            <Carousel.Item interval={500}>
+              <img src={picture} className="Detail-img" />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+        <h3 className="description-text">{apartment.description}</h3>
+        <div className="details-icons">
+          {apartment.amenities.map((amenity) => (
+            <div className="amenities" key={amenity._id} style={{ fontSize: "15px" }, { color: "black" }}>
+              <i className={amenity.icon} /> <span>{amenity.name} </span>
+            </div>
+          ))}
+        </div>
+        <p>Property has a capacity of: {apartment.maxGuests} guests</p>
+        <p>Price: {apartment.pricePerDay}kr / 24 hours</p>
+        <p>The landlord's name: {apartment.ownerId.fullName}</p>
+        <div className="details-calendar">
+          <DetailsCalendar apartmentId={apartment._id} />
+      </div>
+      <> { (calendarDates.length > 0) &&
         <>
-          {" "}
-          <div className="Details">
-            <h2>{apartment.city} </h2>
-            <p> {apartment.region}</p>
-
-            <div className="gallery">
-              <Carousel fade>
-                {apartment.gallery.map((picture) => (
-                  <Carousel.Item interval={500}>
-                    <img src={picture} className="Detail-img" />
-                  </Carousel.Item>
-                ))}
-              </Carousel>
-            </div>
-
-            <h3 className="description-text">{apartment.description}</h3>
-            <p>Property has a capacity of: {apartment.maxGuests} guests</p>
-            <p>Price: {apartment.pricePerDay}kr / 24 hours</p>
-            <p>The landlord's name: {apartment.ownerId.fullName}</p>
-            <div align="right" className="btn-div-details">
-              <button className="next-btn-details" onClick={goToCheckIn}>
-                Next
-              </button>
-            </div>
-          </div>{" "}
-        </>
-      )}
+        <div align="right" className="btn-div-details">
+          <button className="next-btn-details" onClick={goToCheckIn}>Next</button>
+        </div>
+      </>
+      }</>
+      </div>
+</>
+    )}
     </>
-  );
-}   
+  )
+}
