80a2dae7d8daff7acf9f5efb57e4e4df03c23887
Alexander S <70532114+autumnjava@users.noreply.github.com>
2021-03-24 10:53:08 +0100

    css fix
    Co-Authored-By: JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>

--- src/components/apartmentListing/index.jsx
+++ src/components/apartmentListing/index.jsx
@@ -6,32 +6,52 @@ import { MyCalendar } from './calendar'
 import styled from 'styled-components'
 
 
-const PublishButton = styled.button`
-  background-color: #2e2b31;
-  border-radius: 10px;
-  border: 4px double #cccccc;
-  color: #eeeeee;
-  text-align: center;
-  font-size: 28px;
-  padding: 20px;
-  width: 200px;
-  -webkit-transition: all 0.4s;
-  -moz-transition: all 0.4s;
-  -o-transition: all 0.4s;
-  transition: all 0.4s;
-  cursor: pointer;
-  margin: 5px;
-  justify-content: center;
-
-  &:hover {
-  background-color: rgb(90, 90, 90);
-  }
-`
-
 const Form = styled.form`
 margin: 10px auto;
 text-align: center;
 padding: 10px;
+`
+
+const PublishButton = styled.button`
+background-color: #2e2b31;
+border-radius: 10px;
+border: 4px double #cccccc;
+color: #eeeeee;
+text-align: center;
+font-size: 28px;
+padding: 20px;
+width: 200px;
+-webkit-transition: all 0.4s;
+-moz-transition: all 0.4s;
+-o-transition: all 0.4s;
+transition: all 0.4s;
+cursor: pointer;
+margin: 5px;
+justify-content: center;
+&:hover {
+background-color: rgb(90, 90, 90);
+}
+`
+
+const ListingDiv = styled.div`
+margin-left: 10px;
+width: 50%;
+`
+
+const AptWrap = styled.div`
+margin: 25px auto;
+width: 100%;
+height: 10%;
+border: 1px solid lightgray;
+border-radius: 8px;
+
+  input {
+  width: 100%;
+  margin: 5px auto;
+  outline: none;
+  border: none;
+  padding: px;
+    }
 `
 
 export function CreateNewApartment() {
@@ -40,15 +60,16 @@ export function CreateNewApartment() {
   return (
     <div className="container">
       <Form>
-        <div className="listing-div">
+        <ListingDiv>
           <h1>List new Apartment</h1>
-          <div className="apartment-input-wrap"><input placeholder="Title:" /></div>
-          <div className="apartment-input-wrap"><input placeholder="Description:" /></div>
-          <div className="apartment-input-wrap"><input placeholder="Region:" /></div>
-          <div className="apartment-input-wrap"><input placeholder="City:" /></div>
-          <div className="apartment-input-wrap"><input placeholder="Max Guests:" /></div>
-          <div className="apartment-input-wrap"><input placeholder="Price/Night:" /></div>
-        </div>
+          <AptWrap><input placeholder="Title:" /></AptWrap>
+          <AptWrap><input placeholder="Description:" /></AptWrap>
+          <AptWrap><input placeholder="Region:" /></AptWrap>
+          <AptWrap><input placeholder="City:" /></AptWrap>
+          <AptWrap><input placeholder="Max Guests:" /></AptWrap>
+          <AptWrap><input placeholder="Price/Night:" /></AptWrap>
+        </ListingDiv>
+
         <div className="container">
           <h5>Amenities: </h5>
           <div className="row">
