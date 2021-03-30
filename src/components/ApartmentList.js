0bea8a1bf267d481b5fd0c8f3305ca47fe55da4b
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-03-30 20:25:07 +0200

    Fixing media queries for apartment search

--- src/components/ApartmentList.js
+++ src/components/ApartmentList.js
@@ -10,17 +10,19 @@ const ApartmentList = ({ apartments }) => {
         <div
           onClick={() => history.push('/details/' + apartment._id)}
           className="apartment-preview" key={apartment._id}>
-          <div className="img-container">
+          <div className="img-containerApartmentSearch">
             <img src={apartment.gallery[0]} alt="" className="apartment-img" />
           </div>
           <div className="apartment-description">
             <h3>{apartment.title} | {apartment.city}</h3>
-            <p>{apartment.description}</p>
+            <p className="apartmentDisc">{apartment.description}</p>
+            <div className="amenitiesWrapper">
             {apartment.amenities.map((amenity) => (
-              <div className="amenities" key={amenity._id} style={{ fontSize: "15px" }, { color: "rgb(82, 82, 82)"}}>
+              <div className="amenitiesApartmentSearch" key={amenity._id} style={{ fontSize: "15px" }, { color: "rgb(82, 82, 82)"}}>
                 <i className={amenity.icon}/> <span>{amenity.name} </span>
               </div>
             ))}
+            </div>
           </div>
         </div>
       ))}
